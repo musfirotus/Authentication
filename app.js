4f064dfc21c68be716e87e49c84bfb31af361c29
Musfirotus <musfirotus@gmail.com>
2020-08-08 21:43:13 +0700

    app

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000
 
 const routeIndex = require('./src/routes/index')
 const routeAuthor = require('./src/routes/authors')
+const routePost = require('./src/routes/posts')
+const routeComment = require('./src/routes/comments')
 
 app.use(
     bodyParser.urlencoded({
@@ -15,6 +17,8 @@ app.use(bodyParser.json())
 
 app.use('/', routeIndex)
 app.use('/author', routeAuthor)
+app.use('/post', routePost)
+app.use('/comment', routeComment)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)

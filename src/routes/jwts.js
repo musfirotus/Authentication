14f0597a1085377a8dc38d42e54b1687f0227eb1
Musfirotus <musfirotus@gmail.com>
2020-08-10 08:19:04 +0700

    move require

--- src/routes/jwts.js
+++ src/routes/jwts.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// const jwt = require('jsonwebtoken')
-
 const jwtController = require('../controllers/jwtController')
 
 router.post('/login', jwtController.login);
